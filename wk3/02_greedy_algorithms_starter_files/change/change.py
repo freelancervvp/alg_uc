640641ffe5d4b6aa7cf95035bdabb4d441909608
freelancervvp <freelancervvp@gmail.com>
2016-07-29 20:51:29 +0300

    -

--- wk3/02_greedy_algorithms_starter_files/change/change.py
+++ wk3/02_greedy_algorithms_starter_files/change/change.py
@@ -1,10 +1,25 @@
 # Uses python3
 import sys
+import numpy
 
 def get_change(m):
-    #write your code here
-    return m
-
+	#write your code here
+	#print(next(coins))
+	coin = next(coins)
+	#print(m)
+	res = m // coin
+	#print(res)
+	change.append(res)
+	m = m % coin
+	if m == 0:
+		return None
+	get_change(m)
+	return None
+	
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+	m = int(sys.stdin.read())
+	coins = iter([10, 5, 1])
+	change = list()
+	get_change(m)
+	#print(change)
+	print(sum(change))
--- wk3/02_greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.py
+++ wk3/02_greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.py
@@ -1,17 +1,35 @@
 # Uses python3
 import sys
+import numpy as np
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
-
-    return value
+	value = 0.
+	# write your code here
+	n = len(weights)
+	items = np.zeros(n)
+	weights = np.array(weights)
+	values = np.array(values)
+	v2w = values / weights
+	id_sort = np.argsort(v2w)[::-1]
+	#print('id_sort: ', id_sort)
+	for i in id_sort:
+		if capacity == 0:
+			return value, items
+		vol = min(weights[i], capacity)
+		value += vol * v2w[i]
+		weights[i] -= vol
+		items[i] += vol
+		capacity -= vol
+	return value, items
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+	data = list(map(int, sys.stdin.read().split()))
+	n, capacity = data[0:2]
+	values = data[2:(2 * n + 2):2]
+	weights = data[3:(2 * n + 2):2]
+	#print(type(values))
+	opt_value, opt_items = get_optimal_value(capacity, weights, values)
+	#opt_value = round(opt_value, 4)
+	print("{:.10f}".format(opt_value))
+	#print(opt_items)
